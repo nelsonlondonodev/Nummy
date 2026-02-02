39aa4bf15df8cceb883d1c7060eeaaf8361eb94e
Nelson Londoño <nelsonlondonodev@gmail.com>
2026-02-02 21:50:24 +0100

    Enhance UX/UI: Remove deprecated phone/scroll buttons and add modern floating WhatsApp button with pulse animation

--- js/main.js
+++ js/main.js
@@ -69,18 +69,7 @@
     }
   });
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-  $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-    return false;
-  });
+
 
   // Header carousel
   $(".header-carousel").owlCarousel({
